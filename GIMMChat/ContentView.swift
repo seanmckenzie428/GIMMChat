7a2e0a56ba28a5f3436cb7cfa2ec7e90692c2b9f
Sean McKenzie <70048908+seanmckenzie428@users.noreply.github.com>
2023-11-10 14:14:47 -0700

    Initial Commit

---
+++ GIMMChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GIMMChat
+//
+//  Created by Sean McKenzie on 11/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GIMMChat/GIMMChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIMMChatApp.swift
+//  GIMMChat
+//
+//  Created by Sean McKenzie on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GIMMChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
